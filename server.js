49d50263ca11be0256371d8ca51f445c8d393b78
Paige Boyer <paige1381@gmail.com>
2018-06-19 15:18:45 -0700

    created searchable map

--- server.js
+++ server.js
@@ -1,11 +1,37 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const dotenv = require('dotenv').config();
 
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 app.use(express.static('public'));
 
 
+app.get('/', (req, res) => {
+  res.send('working');
+});
 
+app.get('/foodTrucks', (req, res) => {
+  res.render('index.ejs', {
+    embedMap: 'https://www.google.com/maps/embed/v1/place?key=' + process.env.KEY + '&q=San+Francisco'
+  });
+});
+
+app.post('/foodTrucks', (req, res) => {
+  try {
+    res.redirect('/foodTrucks/' + req.body.searchPhrase);
+  }
+  catch (err) {
+    res.send(err.message);
+  }
+});
+
+app.get('/foodTrucks/:searchPhrase', (req, res) => {
+  res.render('index.ejs', {
+    embedMap: 'https://www.google.com/maps/embed/v1/place?key=' + process.env.KEY + '&q=' + req.params.searchPhrase
+  });
+});
 
 
 
